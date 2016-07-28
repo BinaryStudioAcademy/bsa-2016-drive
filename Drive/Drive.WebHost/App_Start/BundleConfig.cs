6126ea8313e02485aea41bd0b97ae8cd0ba2b698
definetux <goncharuk.m.n@gmail.com>
2016-07-28 14:05:51 +0300

    Added solution

---
+++ Drive/Drive.WebHost/App_Start/BundleConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Optimization;
+
+namespace Drive.WebHost
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/Libs/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/Libs/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/Libs/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Scripts/App/"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/Libs/bootstrap.js",
+                "~/Scripts/Libs/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/site.css"));
+        }
+    }
+}
