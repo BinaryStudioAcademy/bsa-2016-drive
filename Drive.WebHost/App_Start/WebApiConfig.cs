d7458796189da5ee4e79598ca36b947b4df51d6b
Olena Rehush <olena_rehush@ukr.net>
2016-08-02 21:54:44 +0300

    Fixed web api routing, added empty user controller.

--- Drive.WebHost/App_Start/WebApiConfig.cs
+++ Drive.WebHost/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace Drive.WebHost
@@ -9,6 +10,7 @@ namespace Drive.WebHost
     {
         public static void Register(HttpConfiguration config)
         {
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
             // Web API configuration and services
 
             // Web API routes
@@ -16,8 +18,8 @@ namespace Drive.WebHost
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = RouteParameter.Optional, id = RouteParameter.Optional }
             );
         }
     }
