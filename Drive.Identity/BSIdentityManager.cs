6a9a36559d94d7a224890879cc9db5aa5c3b64fe
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-03 15:03:36 +0300

    #6: Implemented BSIdentityManager class

---
+++ Drive.Identity/BSIdentityManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drive.Identity
+{
+    public class BSIdentityManager : GenericIdentityManager<BSIdentity>
+    {
+        public int UserId => Identity.UserId;
+        public string Token => Identity.Token;
+        public string Name => Identity.Name;
+    }
+}
