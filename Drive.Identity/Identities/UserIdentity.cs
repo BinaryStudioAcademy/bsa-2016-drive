7edb28d6cb73146f68832aca65488383c665dacc
yetanothergitacc <milkylizard@gmail.com>
2016-08-03 13:10:33 +0300

    Add identity package, IdentityService

---
+++ Drive.Identity/Identities/UserIdentity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+
+namespace Drive.Identity.Identities
+{
+    class UserIdentity : IUser
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public UserIdentity(string userName)
+        {
+            Id = Guid.NewGuid().ToString();
+            UserName = userName;
+        }
+    }
+}
