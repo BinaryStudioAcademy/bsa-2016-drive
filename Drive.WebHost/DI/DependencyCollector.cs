c77d483bee3893d4be47f8f09bc4aa2be20476cd
Mykhailo Voronovskii <Mykhailoblack@ukr.net>
2016-08-03 18:27:40 +0300

    Add Ninject.

---
+++ Drive.WebHost/DI/DependencyCollector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Drive.Core.DI;
+using Drive.DataAccess.DI;
+using Drive.Identity.DI;
+using Ninject;
+
+namespace Drive.WebHost.DI
+{
+    public class DependencyCollector
+    {
+        private IKernel _kernel;
+
+        public DependencyCollector()
+        {
+            //Load modules
+            _kernel = new StandardKernel(new CoreModule(), new DataAccessModule(), new IdentityModule());
+        }
+    }
+}
---
+++ Drive.WebHost/DI/DependencyResolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace Drive.WebHost.DI
+{
+    public class DependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public DependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            //kernel.Bind<IWebInterface>().To<WebClass>();
+        }
+    }
+}
