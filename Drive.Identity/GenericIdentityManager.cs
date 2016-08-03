e4ab6b0f959fb12a4f41a6dd83c836522db63608
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-03 15:02:47 +0300

    #6: Added GenericIdentityManager class

---
+++ Drive.Identity/GenericIdentityManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drive.Identity
+{
+    public class GenericIdentityManager<T> where T : IIdentity
+    {
+        public T Identity => (T)System.Threading.Thread.CurrentPrincipal.Identity;
+
+        public void SetIdentity(T identity, string[] roles)
+        {
+            var principal = new GenericPrincipal(identity, roles);
+            System.Threading.Thread.CurrentPrincipal = principal;
+        }
+    }
+}
