652dff79dfe68a564ea5f5f6203f4ff5b7d8a70e
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-03 15:02:12 +0300

    #6: Added BSIdentity class

---
+++ Drive.Identity/BSIdentity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drive.Identity
+{
+    public class BSIdentity : IIdentity
+    {
+        public string Name { get; }
+        public string Token { get; set; }
+        public int UserId { get; set; }
+        public string AuthenticationType { get; }
+        public bool IsAuthenticated { get; }
+
+        // TODO: Add fields for identity
+    }
+}
