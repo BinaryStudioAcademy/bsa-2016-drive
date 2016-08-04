02d824b3b047bafcdc410f3cd72317558ef280ba
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-04 12:34:13 +0300

    #15: Implemented IHttpClient in AsyncHttpClient

---
+++ Drive.Core/HttpClient/AsyncHttpClient.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drive.Core.HttpClient
+{
+    public class AsyncHttpClient : IHttpClient, IDisposable
+    {
+        private readonly System.Net.Http.HttpClient _client;
+
+        public AsyncHttpClient()
+        {
+            _client = new System.Net.Http.HttpClient();
+        }
+
+        public async Task<string> GetAsync(string url)
+        {
+            var response = await _client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> PostAsync(string url, string content)
+        {
+            var response = await _client.PostAsync(url, new StringContent(content));
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> PutAsync(string url, string content)
+        {
+            var response = await _client.PutAsync(url, new StringContent(content));
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> DeleteAsync(string url)
+        {
+            var response = await _client.DeleteAsync(url);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _client.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // ~AsyncHttpClient() {
+        //   Dispose(false);
+        // }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
