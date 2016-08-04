c93f0fb77aa4aba7c5beabc1cbf90a69eb4373f2
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-04 19:03:07 +0300

    #6: Added server responce entity

---
+++ Drive.Identity/Entities/BSAuthResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace Drive.Identity.Entities
+{
+    [DataContract]
+    internal class BSAuthResponse
+    {
+        [DataMember(EmitDefaultValue = false, Name = "success")]
+        public bool Success { get; set; }
+
+        [DataMember(EmitDefaultValue = false, Name = "message")]
+        public string Message { get; set; }
+
+        [DataMember(EmitDefaultValue = false, Name = "token")]
+        public string Token { get; set; }
+    }
+}
