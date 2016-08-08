3ac3b213a0c9e0ab9aa93c831da5b9fc7d1527b8
zicar123 <zicarvit@gmail.com>
2016-08-08 17:19:10 +0300

    NLog

---
+++ Drive.Logging/Logging.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace Drive.Logging
+{
+    public class Logging : ILogging
+    {
+        public Logger Log { get; }
+
+        public Logging(Logger log)
+        {
+            LogManager.Configuration = new NLog.Config.XmlLoggingConfiguration(@"C:\FILES\GitHub\bsa-2016-drive\Drive.Logging\NLog.config", true);
+
+            Log = log;
+
+            //using (DriveContext context = new DriveContext())
+            //{
+            //    context.LogEntries.Add(new LogEntry());
+            //    context.SaveChanges();
+            //}
+        }
+    }
+}
