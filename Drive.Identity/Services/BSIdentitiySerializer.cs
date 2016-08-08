fc1e4a4d11d44022d8a3f82387aa1bc61af2b407
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-08 16:58:20 +0300

    #10: Added identity serializer

---
+++ Drive.Identity/Services/BSIdentitiySerializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JWT;
+using Newtonsoft.Json;
+
+namespace Drive.Identity.Services
+{
+    public class BSIdentitiySerializer : IJsonSerializer
+    {
+        public string Serialize(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        public T Deserialize<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
