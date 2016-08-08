99518bd4018032c1b8cc807115a4d7fd504f6e64
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-08 16:57:54 +0300

    #10: Added new auth service

---
+++ Drive.Identity/Services/JWTAuthService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IdentityModel.Tokens;
+using System.Security.Principal;
+using System.ServiceModel.Security.Tokens;
+using System.Text;
+using Drive.Identity.Entities;
+using Drive.Identity.Services.Abstract;
+using JWT;
+
+namespace Drive.Identity.Services
+{
+    public class JWTAuthService : ITokenAuthenticationService
+    {
+        public GenericPrincipal VerifyToken(string token, string secret)
+        {
+            try
+            {
+                var identity = JsonWebToken.DecodeToObject<BSIdentity>(token, secret);
+                identity.Token = token;
+                GenericPrincipal principal = new GenericPrincipal(identity, new[] { identity.Role });
+                return principal;
+            }
+            catch (SignatureVerificationException)
+            {
+                // TODO: Catch error
+                return null;
+            }
+        }
+    }
+}
