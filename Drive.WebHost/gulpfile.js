8cad8d4241d7c64d0d81387b3761f5330239d281
definetux <goncharuk.m.n@gmail.com>
2016-08-08 18:30:43 +0300

    #14. Corrected bundles

--- Drive.WebHost/gulpfile.js
+++ Drive.WebHost/gulpfile.js
@@ -3,6 +3,5 @@ var gulp = require('gulp');
 var bower = require('gulp-bower');
 
 gulp.task('bower', function () {
-    return bower()
-        .pipe(gulp.dest('Dependencies/'));
+    return bower('./bower_components');
 });
