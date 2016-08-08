63b619dfb6ee6e42447f922d0b3bc25840162358
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-08 16:57:04 +0300

    #10: Updated IdentityManager

--- Drive.Identity/Services/Abstract/GenericIdentityManager.cs
+++ Drive.Identity/Services/Abstract/GenericIdentityManager.cs
@@ -4,11 +4,17 @@ namespace Drive.Identity.Services.Abstract
 {
     public abstract class GenericIdentityManager<T> where T : IIdentity
     {
-        public T Identity => (T)System.Threading.Thread.CurrentPrincipal.Identity;
+        public IPrincipal Principal = System.Threading.Thread.CurrentPrincipal;
+        public T Identity => (T)Principal.Identity;
 
         public void SetIdentity(T identity, string[] roles)
         {
             var principal = new GenericPrincipal(identity, roles);
+            System.Threading.Thread.CurrentPrincipal = principal;
+        }
+
+        public void SetPrincipal(IPrincipal principal)
+        {
             System.Threading.Thread.CurrentPrincipal = principal;
         }
     }
--- Drive.Identity/Services/BSIdentityManager.cs
+++ Drive.Identity/Services/BSIdentityManager.cs
@@ -8,6 +8,8 @@ namespace Drive.Identity.Services
         public string UserId => Identity.UserId;
         public string Token => Identity.Token;
         public string Name => Identity.Name;
+        public string Email => Identity.Email;
+        public string Role => Identity.Role;
         public bool IsAuthenticated => Identity.IsAuthenticated;
     }
 }
