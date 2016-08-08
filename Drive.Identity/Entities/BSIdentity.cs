3e084f3f0fd1bd8705675e388a853ed6d53939f5
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-08 16:56:26 +0300

    #10: Updated identity

--- Drive.Identity/Entities/BSIdentity.cs
+++ Drive.Identity/Entities/BSIdentity.cs
@@ -1,14 +1,31 @@
-﻿using System.Security.Principal;
+﻿using System;
+using System.Runtime.Serialization;
+using System.Security.Principal;
+using Newtonsoft.Json;
 
 namespace Drive.Identity.Entities
 {
+    [JsonObject(MemberSerialization.OptIn)]
     public class BSIdentity : IIdentity
     {
         public string Name { get; }
-        public string Token { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("iat")]
+        public string IssuedAt { get; set; }
+
+        [JsonProperty("id")]
         public string UserId { get; set; }
-        public string AuthenticationType { get; }
+
+        [JsonProperty("role")]
+        public string Role { get; set; }
+
+        public string Token { get; set; }
+
         public bool IsAuthenticated { get; }
+        public string AuthenticationType { get; }
 
         // TODO: Add fields for identity
     }
