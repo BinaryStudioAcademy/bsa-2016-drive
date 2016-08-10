abd01e86fb05b4f4c0ca3aecc5f45862380101f5
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-10 10:04:32 +0300

    #9: Added logs controller and service initialization

---
+++ Drive.WebHost/Scripts/App/Logs/logsController.js
@@ -0,0 +1,18 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('driveApp')
+        .controller('LogsController', LogsController);
+
+    LogsController.$inject = ['LogsService'];
+
+    function LogsController() {
+        var vm = this;
+
+        activate();
+
+        function activate() {
+            
+        }
+    }
+})();
---
+++ Drive.WebHost/Scripts/App/Logs/logsService.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('driveApp')
+        .factory('LogsService', HomeService);
+
+    HomeService.$inject = [];
+
+    function HomeService() {
+        var service = {
+
+        };
+
+        return service;
+    }
+})();
