98b504fb50ce8add68420f0524e05b785857eb2a
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-10 10:42:23 +0300

    #9: Added dummy data to logs service

--- Drive.WebHost/Scripts/App/Logs/logsService.js
+++ Drive.WebHost/Scripts/App/Logs/logsService.js
@@ -9,8 +9,96 @@
 
     function HomeService() {
         var service = {
-
+            getLogs: getLogs
         };
+
+        function getLogs() {
+            return [
+            {
+                id: 1,
+                time: new Date(2016, 8, 6, 3, 8, 10),
+                level: 'Error',
+                message: 'Error message 1',
+                exception: 'Exception message 1'
+            },
+            {
+                id: 2,
+                time: new Date(2016, 8, 6, 3, 8, 10),
+                level: 'Info',
+                message: 'Error message 1',
+                exception: 'Exception message 1'
+            },
+            {
+                id: 3,
+                time: new Date(2016, 8, 6, 3, 8, 10),
+                level: 'Warn',
+                message: 'Error message 1',
+                exception: 'Exception message 1'
+            },
+            {
+                id: 4,
+                time: new Date(2016, 8, 6, 3, 8, 10),
+                level: 'Info',
+                message: 'Error message 1',
+                exception: 'Exception message 1'
+            },
+            {
+                id: 5,
+                time: new Date(2016, 8, 6, 3, 8, 10),
+                level: 'Debug',
+                message: 'Error message 1',
+                exception: 'Exception message 1'
+            },
+            {
+                id: 6,
+                time: new Date(2016, 8, 6, 3, 8, 10),
+                level: 'Trace',
+                message: 'Error message 1',
+                exception: 'Exception message 1'
+            },
+            {
+                id: 7,
+                time: new Date(2016, 8, 6, 3, 8, 10),
+                level: 'Error',
+                message: 'Error message 1',
+                exception: 'Exception message 1'
+            },
+            {
+                id: 8,
+                time: new Date(2016, 8, 6, 3, 8, 10),
+                level: 'Info',
+                message: 'Error message 1',
+                exception: 'Exception message 1'
+            },
+            {
+                id: 9,
+                time: new Date(2016, 8, 6, 3, 8, 10),
+                level: 'Warn',
+                message: 'Error message 1',
+                exception: 'Exception message 1'
+            },
+            {
+                id: 10,
+                time: new Date(2016, 8, 6, 3, 8, 10),
+                level: 'Info',
+                message: 'Error message 1',
+                exception: 'Exception message 1'
+            },
+            {
+                id: 11,
+                time: new Date(2016, 8, 6, 3, 8, 10),
+                level: 'Debug',
+                message: 'Error message 1',
+                exception: 'Exception message 1'
+            },
+            {
+                id: 12,
+                time: new Date(2016, 8, 6, 3, 8, 10),
+                level: 'Trace',
+                message: 'Error message 1',
+                exception: 'Exception message 1'
+            }];
+        }
 
         return service;
     }
