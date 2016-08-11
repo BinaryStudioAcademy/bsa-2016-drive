bd9a60710a0de268bb0010c37385812a55494300
vzayats <vzayats3@gmail.com>
2016-08-11 14:38:38 +0300

    #16: Added $location.url in menuController

--- Drive.WebHost/Scripts/App/Menu/menuController.js
+++ Drive.WebHost/Scripts/App/Menu/menuController.js
@@ -3,4 +3,38 @@
 
     angular.module("driveApp")
         .controller("MenuController", MenuController);
+
+    MenuController.$inject = ['MenuService', '$location'];
+
+    function MenuController(menuService) {
+        var vm = this;
+        vm.redirectToBinarySpace = redirectToBinarySpace;
+        vm.redirectToMySpace = redirectToMySpace;
+        vm.redirectToSpaceSettings = redirectToSpaceSettings;
+        vm.redirectToAddBinarySpace = redirectToAddBinarySpace;
+        vm.redirectToAddFile = redirectToAddFile;
+        vm.redirectToAddMySpace = redirectToAddMySpace;
+
+        function redirectToBinarySpace(id) {
+           $location.url('App/Space/Space' + id);
+        };
+
+        function redirectToSpaceSettings(id) {
+            $location.url('App/Space/Settings/' + id);
+        };
+
+        function redirectToAddBinarySpace() {
+            $location.url('App/Space/AddSpace');
+        };
+
+        function redirectToAddFile() {
+            $location.url('App/Space/AddFile');
+        };
+
+        function redirectToAddMySpace() {
+            $location.url('App/Space/AddSpace');
+        };
+
+        
+    }
 }());
---
+++ Drive.WebHost/Scripts/App/Menu/menuService.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('driveApp')
+        .factory('MenuService', MenuService);
+
+    MenuService.$inject = [];
+
+    function MenuService() {
+        var service = {
+            
+        };
+
+        return service;
+    }
+})();
