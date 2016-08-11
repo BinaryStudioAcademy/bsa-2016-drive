beedb23025e3e2fcf3d9ede5bb598afbbb97a4e8
Mykhailo Voronovskii <Mykhailoblack@ukr.net>
2016-08-11 11:17:47 +0300

    Delete unnecessary files.

--- Drive.WebHost/Scripts/App/Folder/modalController.js
+++ Drive.WebHost/Scripts/App/Folder/modalController.js
@@ -1,23 +1,4 @@
-﻿//(function () {
-//    "use strict";
-
-//    angular
-//        .module("driveApp")
-//        .controller("ModalController", ModalController);
-
-//    FolderController.$inject = [];
-
-//    function FolderController() {
-//        var vm = this;
-
-//        activate();
-
-//        function activate() {
-//        }
-//    }
-//}());
-
-angular.module('driveApp').controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+﻿angular.module('driveApp').controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
 
     //$scope.items = items;
     //$scope.selected = {
