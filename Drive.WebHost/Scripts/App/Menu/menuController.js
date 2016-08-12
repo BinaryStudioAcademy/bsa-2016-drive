380b826356951de97bc7d045f2d3d78a0e8a9b75
vzayats <vzayats3@gmail.com>
2016-08-12 05:40:47 +0300

    #16: Added new location.url for menu

--- Drive.WebHost/Scripts/App/Menu/menuController.js
+++ Drive.WebHost/Scripts/App/Menu/menuController.js
@@ -11,14 +11,24 @@
         var vm = this;
 
         vm.redirectToBinarySpace = redirectToBinarySpace;
+        vm.redirectToMySpace = redirectToMySpace;
         vm.redirectToSpaceSettings = redirectToSpaceSettings;
         vm.redirectToAddBinarySpace = redirectToAddBinarySpace;
         vm.redirectToAddFile = redirectToAddFile;
         vm.redirectToAddMySpace = redirectToAddMySpace;
+        vm.redirectToNetSpace = redirectToNetSpace;
+        vm.redirectToDocs = redirectToDocs;
+        vm.redirectToEvents = redirectToEvents;
+        vm.redirectToAdminPanel = redirectToAdminPanel;
+        vm.redirectToUsersSettings = redirectToUsersSettings;
         
         activate();
 
         function redirectToBinarySpace(id) {
+            $location.url('/api/spaces/' + id);
+        };
+
+        function redirectToMySpace(id) {
             $location.url('/api/spaces/' + id);
         };
 
@@ -31,11 +41,31 @@
         };
 
         function redirectToAddFile() {
-            $location.url('api/files');
+            $location.url('api/files/');
         };
 
         function redirectToAddMySpace() {
-            $location.url('api/Space/spaces');
+            $location.url('api/spaces/');
+        };
+
+        function redirectToNetSpace() {
+            $location.url('api/netSpace/');
+        };
+
+        function redirectToDocs() {
+            $location.url('api/docs/');
+        };
+
+        function redirectToEvents() {
+            $location.url('api/events/');
+        };
+
+        function redirectToAdminPanel() {
+            $location.url('api/adminPanel/');
+        };
+
+        function redirectToUsersSettings() {
+            $location.url('api/usersSettings/');
         };
 
         function activate() {
