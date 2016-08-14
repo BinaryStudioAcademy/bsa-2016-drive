7e96b695f1138c62f5825a1d677028c8d927dcac
vzayats <vzayats3@gmail.com>
2016-08-14 14:48:58 +0300

    #35: Added breadcrumbs for navigation

--- Drive.WebHost/Scripts/App/app.js
+++ Drive.WebHost/Scripts/App/app.js
@@ -5,18 +5,18 @@
         function ($routeProvider) {
 
             $routeProvider
-            //.when('/', // Home Page
-            //{
-            //    templateUrl: '/Scripts/App/Space/Space.html',
-            //    controller: 'HomeController',
-            //    controllerAs: 'homeCtrl'
-            //})
-            .when('/', // Space page
+            .when('/', // Home Page
             {
-                templateUrl: '/Scripts/App/Folder/Folder.html',
-                controller: 'FolderController',
-                controllerAs: 'folderCtrl'
+                templateUrl: '/Scripts/App/Space/Space.html',
+                controller: 'HomeController',
+                controllerAs: 'homeCtrl'
             })
+            //.when('/', // Space page
+            //{
+            //    templateUrl: '/Scripts/App/Folder/Folder.html',
+            //    controller: 'FolderController',
+            //    controllerAs: 'folderCtrl'
+            //})
             .when('/settings/', // Space settings Page
             {
                 templateUrl: '/Scripts/App/Space/Settings.html',
@@ -30,11 +30,11 @@
                 controllerAs: 'logsCtrl'
             })
             .when('/Folders', // Folders page
-                   {
-                       templateUrl: '/Scripts/App/Folder/Folder.html',
-                       controller: 'FolderController',
-                       controllerAs: 'folderCtrl'
-                   })
+            {
+                templateUrl: '/Scripts/App/Folder/Folder.html',
+                controller: 'FolderController',
+                controllerAs: 'folderCtrl'
+            })
             .otherwise({ // This is when any route not matched - error
                 controller: 'ErrorController'
             });
