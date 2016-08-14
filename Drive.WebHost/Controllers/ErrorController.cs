67f76696f8a113f4954efa03aec091664c7e70c3
Mykhailo Voronovskii <Mykhailoblack@ukr.net>
2016-08-14 11:40:55 +0300

    Mvc error handling. Local trace enable.

---
+++ Drive.WebHost/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Drive.WebHost.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+
+        public ActionResult InternalServerError()
+        {
+            Response.StatusCode = 500;
+            return View();
+        }
+    }
+}
