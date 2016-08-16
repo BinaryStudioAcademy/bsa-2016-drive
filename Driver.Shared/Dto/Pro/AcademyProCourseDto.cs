479ca3fbb47a158ac2da4bcf52324904b5df6fe9
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-16 17:20:02 +0300

    #75: Added basic DTO for Academy Pro

---
+++ Driver.Shared/Dto/Pro/AcademyProCourseDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Driver.Shared.Dto.Pro
+{
+    public class AcademyProCourseDto
+    {
+        public int Id { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public IList<LectureDto> Lectures { get; set; }
+
+        public FileUnitDto FileUnit { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
---
+++ Driver.Shared/Dto/Pro/LectureDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Driver.Shared.Dto.Pro
+{
+    public class LectureDto
+    {
+        public int Id { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public UserDto Author { get; set; }
+        public IList<ContentLinkDto> ContentList { get; set; }
+
+        public IList<TagDto> Tags { get; set; }
+        public IList<CodeSampleDto> CodeSamples { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
