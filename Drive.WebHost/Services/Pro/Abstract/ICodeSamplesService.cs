d020f0c588c67bec4876577502d81b1c3682d9e7
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-17 17:38:59 +0300

    #75: Added CodeSamplesService interface and implementation

---
+++ Drive.WebHost/Services/Pro/Abstract/ICodeSamplesService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Driver.Shared.Dto.Pro;
+
+namespace Drive.WebHost.Services.Pro.Abstract
+{
+    public interface ICodeSamplesService : IBasicService<CodeSampleDto>
+    {
+    }
+}
