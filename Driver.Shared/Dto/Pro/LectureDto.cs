00df274591b5982c8f8b1db5421cc662c17ecc99
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-17 13:54:33 +0300

    #75: Changed UserDto to AuthorDto in LectureDto

--- Driver.Shared/Dto/Pro/LectureDto.cs
+++ Driver.Shared/Dto/Pro/LectureDto.cs
@@ -8,7 +8,7 @@ namespace Driver.Shared.Dto.Pro
         public int Id { get; set; }
         public bool IsDeleted { get; set; }
 
-        public UserDto Author { get; set; }
+        public AuthorDto Author { get; set; }
 
         public string Name { get; set; }
         public string Description { get; set; }
