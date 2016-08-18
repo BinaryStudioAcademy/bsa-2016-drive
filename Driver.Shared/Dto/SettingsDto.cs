f35c46f6291c7ab22ec7d55024812864829c6ecc
Mykhailo Voronovskii <Mykhailoblack@ukr.net>
2016-08-18 13:13:11 +0300

    Add backkend for space settings.

---
+++ Driver.Shared/Dto/SettingsDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Drive.DataAccess.Entities;
+
+namespace Driver.Shared.Dto
+{
+    public class SettingsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int MaxFilesQuantity { get; set; }
+
+        public int MaxFileSize { get; set; }
+    }
+}
