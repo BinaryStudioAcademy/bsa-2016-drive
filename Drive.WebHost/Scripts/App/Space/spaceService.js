4293e750dbe880ea6b501f90572d2353c0ee14c8
milklim <klimbox@gmail.com>
2016-08-18 21:55:44 +0300

    small refactoring: removed unnecessary files, moved the filter of types to a separate file

--- Drive.WebHost/Scripts/App/Space/spaceService.js
+++ Drive.WebHost/Scripts/App/Space/spaceService.js
@@ -126,28 +126,4 @@
         return service;
     }
 
-    var app = angular.module('driveApp');
-
-    app.filter('typeOfFile', function () {
-        return function (input, uppercase) {
-            switch (input) {
-                case 0:
-                    return 'Undefined';
-                case 1:
-                    return 'Document';
-                case 2:
-                    return 'Sheets';
-                case 3:
-                    return 'Slides';
-                case 4:
-                    return 'Trello';
-                case 5:
-                    return 'Link';
-                case 6:
-                    return 'Physical file';
-                default:
-                    return '';
-            }
-        }
-    });
 })();
