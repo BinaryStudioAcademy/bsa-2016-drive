05e9b5b352f0a009156284f34484eff190eb443f
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-20 12:44:31 +0300

    #87: Added Authenticator class with shared logic

---
+++ Drive.WebHost/Filters/Authenticator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using Drive.Identity.Entities;
+using Drive.Identity.Services;
+using Drive.Identity.Services.Abstract;
+
+namespace Drive.WebHost.Filters
+{
+    public class Authenticator
+    {
+        public static IPrincipal CreatePrincipal(string token)
+        {
+            var secret = ConfigurationManager.AppSettings["JWTSecret"];
+            ITokenAuthenticationService authService = new JWTAuthService();
+            var principal = authService.VerifyToken(token, secret);
+            var checkExpiracy = bool.Parse(ConfigurationManager.AppSettings["CheckExpiracy"]);
+            if (checkExpiracy && ((BSIdentity)principal.Identity).IsExpired)
+            {
+                throw new TokenExpiredException("token expired");
+            }
+            return principal;
+        }
+    }
+}
---
+++ Drive.WebHost/Filters/TokenExpiredException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Drive.WebHost.Filters
+{
+    public class TokenExpiredException : Exception
+    {
+        public TokenExpiredException()
+        {
+        }
+
+        public TokenExpiredException(string message) : base(message)
+        {
+        }
+
+        public TokenExpiredException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected TokenExpiredException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
