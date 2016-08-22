fa3a7191eb897ac7f8d9a2ce15e0a54af1c4b47b
milklim <klimbox@gmail.com>
2016-08-22 16:41:42 +0300

    parsing the file type when it is created

--- Drive.WebHost/Scripts/App/Space/spaceController.js
+++ Drive.WebHost/Scripts/App/Space/spaceController.js
@@ -152,97 +152,40 @@
 
 
         vm.folderMenuOptions = [
-            [
-                'Share', function ($itemScope) {
+            ['Share', function ($itemScope) {
                     console.log($itemScope.folder.id);
-                }
-            ],
-            [
-                'Edit', function ($itemScope) {
+                }],
+            ['Edit', function ($itemScope) {
                     vm.folder = $itemScope.folder;
                     vm.folder.parentId = vm.parentId;
                     vm.folder.spaceId = vm.spaceId;
                     vm.openFolderWindow();
-                }
-            ],
-            [
-                'Delete', function ($itemScope) {
+                }],
+            ['Delete', function ($itemScope) {
                     return deleteFolder($itemScope.folder.id);
-                }
-            ]
+                }]
         ];
 
         vm.fileMenuOptions = [
-            [
-                'Share', function ($itemScope) {
+            ['Share', function ($itemScope) {
                     console.log($itemScope.file.id);
-                }
-            ],
-            [
-                'Edit', function ($itemScope) {
+                }],
+            ['Edit', function ($itemScope) {
                     vm.file = $itemScope.file;
                     vm.file.parentId = vm.parentId;
                     vm.file.spaceId = vm.spaceId;
                     vm.openFileWindow();
-                }
-            ],
-            [
-                'Delete', function ($itemScope) {
+                }],
+            ['Delete', function ($itemScope) {
                     return deleteFile($itemScope.file.id);
-                }
-            ]
+                }]
         ];
 
         vm.createOption = [
-            [
-                'Create folder', function () {
-                    vm.folder = { parentId: vm.parentId, spaceId: vm.spaceId };
-                    vm.openFolderWindow();
-                }
-            ],
-            [
-                'Create file', function ($itemScope) {
-                },
-                [
-                    [
-                        'Document', function () {
-                            vm.file = { fileType: 1, parentId: vm.parentId, spaceId: vm.spaceId };
-                            vm.openFileWindow();
-                        }
-                    ],
-                    [
-                        'Sheets', function ($itemScope) {
-                            vm.file = { fileType: 2, parentId: vm.parentId, spaceId: vm.spaceId };
-                            vm.openFileWindow();
-                        }
-                    ],
-                    [
-                        'Slides', function ($itemScope) {
-                            vm.file = { fileType: 3, parentId: vm.parentId, spaceId: vm.spaceId };
-                            vm.openFileWindow();
-                        }
-                    ],
-                    [
-                        'Trello', function ($itemScope) {
-                            vm.file = { fileType: 4, parentId: vm.parentId, spaceId: vm.spaceId };
-                            vm.openFileWindow();
-                        }
-                    ],
-                    [
-                        'Link', function ($itemScope) {
-                            vm.file = { fileType: 5, parentId: vm.parentId, spaceId: vm.spaceId };
-                            vm.openFileWindow();
-                        }
-                    ],
-                    null,
-                    [
-                        'Upload file', function ($itemScope) {
-                            vm.file = { fileType: 6, parentId: vm.parentId, spaceId: vm.spaceId };
-                            vm.openFileWindow();
-                        }
-                    ]
-                ]
-            ]
+            ['Create folder', function () { vm.createNewFolder(); }],
+            ['Create file', function () { vm.createNewFile(); }],
+            null,
+            ['Upload file', function(){}]
         ];
 
         function openFolderWindow(size) {
@@ -324,8 +267,8 @@
             vm.openFolderWindow();
         }
 
-        function createNewFile(type) {
-            vm.file = { fileType: type, parentId: vm.parentId, spaceId: vm.spaceId };
+        function createNewFile() {
+            vm.file = {parentId: vm.parentId, spaceId: vm.spaceId };
             vm.openFileWindow();
         }
 
