31b9b934843475e98281207087ca8c38f5835895
vzayats <vzayats3@gmail.com>
2016-08-22 17:34:34 +0300

    #176: Added remote header

---
+++ Drive.WebHost/Scripts/App/Navbar/header.js
@@ -0,0 +1,17 @@
+﻿var getHeader = function () {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'http://team.binary-studio.com/app/header', true);
+    request.send();
+    request.onreadystatechange = function () {
+        if (request.readyState != 4) return;
+        if (request.status != 200) {
+            alert(request.status + ': ' + request.statusText);
+        } else {
+            var headerHtml = request.responseText;
+            var headerContainer = document.getElementById('header');
+            headerContainer.innerHTML = headerHtml;
+            headerFunction();
+        }
+    };
+};
+getHeader();
