9f5eba0d2dd99090b20e705360eb3854efc4107a
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-23 12:20:23 +0300

    #76: Implemented academyListService

--- Drive.WebHost/Scripts/App/Academy/List/academyListService.js
+++ Drive.WebHost/Scripts/App/Academy/List/academyListService.js
@@ -5,9 +5,42 @@
         .module('driveApp.academyPro')
         .factory('AcademyListService', AcademyListService);
 
-    AcademyListService.$inject = [];
+    AcademyListService.$inject = ['$http'];
 
-    function AcademyListService() {
+    function AcademyListService($http) {
+        var service = {
+            getAcademies: getAcademies,
+            pushData: pushData
+        }
 
+        return service;
+
+        function getAcademies() {
+            return $http.get('/api/academies')
+                .then(getAcademiesCompleted)
+                .catch(getAcademiesFailed);
+
+            function getAcademiesCompleted(response) {
+                return response.data;
+            }
+
+            function getAcademiesFailed(error) {
+                console.log('XHR Failed for getAcademies.' + error.data);
+            }
+        }
+
+        function pushData(data) {
+            return $http.post('/api/academies', data)
+                .then(pushDataCompleted)
+                .catch(pushDataFailed);
+
+            function pushDataCompleted(response) {
+                return response.data;
+            }
+
+            function pushDataFailed(error) {
+                console.log('XHR Failed for pushData.' + error.data);
+            }
+        }
     }
 })();
