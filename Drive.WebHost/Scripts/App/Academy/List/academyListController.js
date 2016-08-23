80cec4a508c9c3e0e4e8f885318949ad6de02893
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-23 21:05:08 +0300

    #76: Implemented basic courses list front service

--- Drive.WebHost/Scripts/App/Academy/List/academyListController.js
+++ Drive.WebHost/Scripts/App/Academy/List/academyListController.js
@@ -4,13 +4,29 @@
     angular.module('driveApp.academyPro')
         .controller('AcademyListController', AcademyListController);
 
-    function AcademyListController() {
+    AcademyListController.$inject = ['AcademyListService', '$location'];
+
+    function AcademyListController(academyListService, $location) {
         var vm = this;
+        vm.academiesList = [];
+        vm.openCourse = openCourse;
 
         activate();
 
         function activate() {
-            vm.title = "Academy Pro";
+            return getAcademies();
+        }
+
+        function getAcademies() {
+            return academyListService.getAcademies()
+                .then(function(data) {
+                    vm.academiesList = data;
+                    return vm.academiesList;
+                });
+        }
+
+        function openCourse(id) {
+            $location.url('/apps/academy/' + id);
         }
     }
 }());
