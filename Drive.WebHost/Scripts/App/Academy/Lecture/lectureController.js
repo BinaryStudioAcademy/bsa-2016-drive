32847bd49037f06d3d08c0630bd8e60691c90ea6
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-23 21:04:52 +0300

    #76: Implemented basic lectures front service

--- Drive.WebHost/Scripts/App/Academy/Lecture/lectureController.js
+++ Drive.WebHost/Scripts/App/Academy/Lecture/lectureController.js
@@ -4,13 +4,25 @@
     angular.module('driveApp.academyPro')
         .controller('LectureController', LectureController);
 
-    function LectureController() {
+    LectureController.$inject = ['LectureService', '$routeParams'];
+
+    function LectureController(lectureService, $routeParams) {
         var vm = this;
+        vm.currentLectureId = $routeParams.lectureId;
+        vm.lecture = null;
 
         activate();
 
         function activate() {
-            vm.title = "Academy Pro";
+            return getLecture();
+        }
+
+        function getLecture() {
+            return lectureService.getLecture(vm.currentLectureId)
+                .then(function (data) {
+                    vm.lecture = data;
+                    return vm.lecture;
+                });
         }
     }
 }());
