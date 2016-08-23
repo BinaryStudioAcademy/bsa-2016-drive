b3a1b11bb79beb5be8a04bf788ae1900f9583dde
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-23 21:05:25 +0300

    #76: Implemented basic course front service

--- Drive.WebHost/Scripts/App/Academy/Single/academyController.js
+++ Drive.WebHost/Scripts/App/Academy/Single/academyController.js
@@ -4,13 +4,30 @@
     angular.module('driveApp.academyPro')
         .controller('AcademyController', AcademyController);
 
-    function AcademyController() {
+    AcademyController.$inject = ['AcademyService', '$location', '$routeParams'];
+
+    function AcademyController(academyService, $location, $routeParams) {
         var vm = this;
+        vm.currentAcademyId = $routeParams.id;
+        vm.academy = null;
+        vm.openLecture = openLecture;
 
         activate();
 
         function activate() {
-            vm.title = "Academy Pro";
+            return getAcademy();
+        }
+
+        function getAcademy() {
+            return academyService.getAcademy(vm.currentAcademyId)
+                .then(function (data) {
+                    vm.academy = data;
+                    return vm.academy;
+                });
+        }
+
+        function openLecture(id) {
+            $location.url('/apps/academy/' + vm.currentAcademyId + '/lecture/' + id);
         }
     }
 }());
