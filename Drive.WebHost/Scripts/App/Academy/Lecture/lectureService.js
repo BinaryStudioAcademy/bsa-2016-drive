ecbfb272c5859ce5044d686eef54790e7074b00e
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-23 12:20:02 +0300

    #76: Implemented lecture service

--- Drive.WebHost/Scripts/App/Academy/Lecture/lectureService.js
+++ Drive.WebHost/Scripts/App/Academy/Lecture/lectureService.js
@@ -5,9 +5,42 @@
         .module('driveApp.academyPro')
         .factory('LectureService', LectureService);
 
-    LectureService.$inject = [];
+    LectureService.$inject = ['$http'];
 
-    function LectureService() {
+    function LectureService($http) {
+        var service = {
+            getLecture: getLecture,
+            pushData: pushData
+        }
 
+        return service;
+
+        function getLecture(id) {
+            return $http.get('/api/lectures/' + id)
+                .then(getLectureCompleted)
+                .catch(getLectureFailed);
+
+            function getLectureCompleted(response) {
+                return response.data;
+            }
+
+            function getLectureFailed(error) {
+                console.log('XHR Failed for getLecture.' + error.data);
+            }
+        }
+
+        function pushData(data) {
+            return $http.post('/api/lectures', data)
+                .then(pushDataCompleted)
+                .catch(pushDataFailed);
+
+            function pushDataCompleted(response) {
+                return response.data;
+            }
+
+            function pushDataFailed(error) {
+                console.log('XHR Failed for pushData.' + error.data);
+            }
+        }
     }
 })();
