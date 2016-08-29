400e5a2b1564d58cc2aa95c9ecf2575b59c37a4c
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-29 14:44:26 +0300

    #76: Added view ang logic for lecture creation

--- Drive.WebHost/Scripts/App/Academy/Lecture/lectureCreateController.js
+++ Drive.WebHost/Scripts/App/Academy/Lecture/lectureCreateController.js
@@ -4,20 +4,34 @@
 
     LectureCreateController.$inject = [
         'LectureService',
-        'toastr'
+        'toastr',
+        '$location',
+        '$routeParams'
     ];
 
-    function LectureCreateController(lectureService, toastr) {
+    function LectureCreateController(lectureService, toastr, $location, $routeParams) {
         var vm = this;
+        vm.currentAcademyId = $routeParams.id;
         vm.addNewLecture = addNewLecture;
         vm.create = create;
         vm.cancel = cancel;
+        vm.submitVideo = submitVideo;
+        vm.submitSlide = submitSlide;
+        vm.submitRepository = submitRepository;
+        vm.submitSample = submitSample;
+        vm.submitUseful = submitUseful;
 
         activate();
 
         function activate() {
             vm.lecture = {
-
+                courseId: vm.currentAcademyId,
+                videoLinks: [],
+                slidesLinks: [],
+                sampleLinks: [],
+                usefulLinks: [],
+                repositoryLinks: [],
+                codeSamples: []
             };
 
             vm.calendar = {
@@ -30,17 +44,20 @@
         }
 
         function addNewLecture() {
-            if (vm.lecture.name !== null) {
-                lectureService.pushData(vm.course);
-            }
+            return lectureService.pushData(vm.lecture);
         };
 
         function create() {
-            vm.addNewLecture();
+            if (vm.lecture.name) {
+                vm.addNewLecture()
+                    .then(function() {
+                        $location.url('/apps/academy/' + vm.currentAcademyId);
+                    });
+            }
         }
 
         function cancel() {
-            
+
         }
 
         function openCalendar(e) {
@@ -49,5 +66,40 @@
 
             vm.calendar.isOpen = true;
         };
+
+        function submitVideo() {
+            if (vm.currentVideo.name && vm.currentVideo.link) {
+                vm.lecture.videoLinks.push(vm.currentVideo);
+                vm.currentVideo = {};
+            }
+        }
+
+        function submitSlide() {
+            if (vm.currentSlide.name && vm.currentSlide.link) {
+                vm.lecture.slidesLinks.push(vm.currentSlide);
+                vm.currentSlide = {};
+            }
+        }
+
+        function submitRepository() {
+            if (vm.currentRepository.name && vm.currentRepository.link) {
+                vm.lecture.repositoryLinks.push(vm.currentRepository);
+                vm.currentRepository = {};
+            }
+        }
+
+        function submitSample() {
+            if (vm.currentSample.name && vm.currentSample.link) {
+                vm.lecture.sampleLinks.push(vm.currentSample);
+                vm.currentSample = {};
+            }
+        }
+
+        function submitUseful() {
+            if (vm.currentuseful.name && vm.currentuseful.link) {
+                vm.lecture.usefulLinks.push(vm.currentuseful);
+                vm.currentuseful = {};
+            }
+        }
     }
 })();
