7f32313ca330424f4986637c46782ed8aea9fdd0
Artur Brutjan <artur.brutjan7@gmail.com>
2016-08-29 10:52:02 +0300

    #76: Added lecture create emty controller and view

---
+++ Drive.WebHost/Scripts/App/Academy/Lecture/lectureCreateController.js
@@ -0,0 +1,53 @@
+﻿(function () {
+    angular.module('driveApp.academyPro')
+        .controller('LectureCreateController', LectureCreateController);
+
+    LectureCreateController.$inject = [
+        'LectureService',
+        'toastr'
+    ];
+
+    function LectureCreateController(lectureService, toastr) {
+        var vm = this;
+        vm.addNewLecture = addNewLecture;
+        vm.create = create;
+        vm.cancel = cancel;
+
+        activate();
+
+        function activate() {
+            vm.lecture = {
+
+            };
+
+            vm.calendar = {
+                isOpen: false,
+                openCalendar: openCalendar,
+                timepickerOptions: {
+                    showMeridian: false
+                }
+            }
+        }
+
+        function addNewLecture() {
+            if (vm.lecture.name !== null) {
+                lectureService.pushData(vm.course);
+            }
+        };
+
+        function create() {
+            vm.addNewLecture();
+        }
+
+        function cancel() {
+            
+        }
+
+        function openCalendar(e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            vm.calendar.isOpen = true;
+        };
+    }
+})();
