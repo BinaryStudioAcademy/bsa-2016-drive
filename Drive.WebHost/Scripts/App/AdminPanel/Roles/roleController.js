dfcc8b4b9671cfe85bccda5dfb4ef015979ffc36
Nikita Krasnov <nikita-krasnov-nk@yandex.ru>
2016-08-31 13:01:28 +0300

    #64: Fixed updating list of roles after creating new role

--- Drive.WebHost/Scripts/App/AdminPanel/Roles/roleController.js
+++ Drive.WebHost/Scripts/App/AdminPanel/Roles/roleController.js
@@ -4,9 +4,9 @@
     angular.module("driveApp")
         .controller("RoleController", RoleController);
 
-    RoleController.$inject = ['$uibModalInstance', 'RoleService', 'items', '$window', 'toastr'];
+    RoleController.$inject = ['$uibModalInstance', 'RoleService', 'items', '$window', 'toastr', 'parentvm'];
 
-    function RoleController($uibModalInstance, RoleService, items, $window, toastr) {
+    function RoleController($uibModalInstance, RoleService, items, $window, toastr, parentvm) {
         var vm = this;
         vm.createRole = createRole;
         vm.cancel = cancel;
@@ -55,10 +55,15 @@
 
         function createRole() {
             console.log('Creating role');
+            vm.name = vm.role.name;
             vm.role.users = vm.role.users || [];
             vm.role.users = vm.permittedUsers;
-            RoleService.createRole(vm.role);
-            $uibModalInstance.close();
+            RoleService.createRole(vm.role, function (data) {
+                vm.id = data;
+                parentvm.roleName = vm.name;
+                parentvm.roleId = vm.id;
+                $uibModalInstance.close();
+            });
         }
 
         function save() {
