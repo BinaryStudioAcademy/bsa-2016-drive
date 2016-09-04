7696d49d1673ada5f1b3f9dab55c9c834bd6a783
milklim <klimbox@gmail.com>
2016-09-04 23:20:51 +0300

    TrashBin fix: added new trashBinDto-s to project

---
+++ Driver.Shared/Dto/TrashBin/TrashBinDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Driver.Shared.Dto.TrashBin
+{
+    public class TrashBinDto
+    {
+        public int SpaceId { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<TrashBinFolderDto> Folders { get; set; }
+        public IEnumerable<TrashBinFileDto> Files { get; set; }
+    }
+}
