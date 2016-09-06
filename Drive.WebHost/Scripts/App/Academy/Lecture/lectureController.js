19dc71213c73b0cbec18e524daf1181a3b00c060
Artur Brutjan <artur.brutjan7@gmail.com>
2016-09-06 22:20:03 +0300

    #379: Frontend logic for lecture editing implemented

--- Drive.WebHost/Scripts/App/Academy/Lecture/lectureController.js
+++ Drive.WebHost/Scripts/App/Academy/Lecture/lectureController.js
@@ -4,9 +4,15 @@
     angular.module('driveApp.academyPro')
         .controller('LectureController', LectureController);
 
-    LectureController.$inject = ['LectureService', '$routeParams', '$sce', 'LinkTypeService'];
+    LectureController.$inject = [
+        'LectureService',
+        '$routeParams',
+        '$sce',
+        'LinkTypeService',
+        'toastr'
+    ];
 
-    function LectureController(lectureService, $routeParams, $sce, linkTypeService) {
+    function LectureController(lectureService, $routeParams, $sce, linkTypeService, toastr) {
         var vm = this;
         vm.currentLectureId = $routeParams.lectureId;
         vm.lecture = null;
@@ -15,11 +21,26 @@
         vm.update = update;
         vm.updateLecture = updateLecture;
         vm.linkTypes = linkTypeService;
+        vm.getLecture = getLecture;
+        vm.submitVideo = submitVideo;
+        vm.submitSlide = submitSlide;
+        vm.submitRepository = submitRepository;
+        vm.submitSample = submitSample;
+        vm.submitUseful = submitUseful;
 
         activate();
 
         function activate() {
             vm.isEditing = false;
+
+            vm.calendar = {
+                isOpen: false,
+                openCalendar: openCalendar,
+                timepickerOptions: {
+                    showMeridian: false
+                }
+            }
+
             return getLecture();
         }
 
@@ -36,11 +57,14 @@
         }
 
         function edit() {
-            vm.isEditing = true;
+            vm.isEditing = !vm.isEditing;
         }
 
         function update() {
-            return lectureService.pushData(vm.lecture);
+            return lectureService.putData(vm.currentLectureId, vm.lecture)
+            .then(function() {
+                vm.isEditing = !vm.isEditing;
+            });
         }
 
         function updateLecture() {
@@ -54,6 +78,48 @@
                 {
                     closeButton: true, timeOut: 6000
                 });
+            }
+        }
+
+        function openCalendar(e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            vm.calendar.isOpen = true;
+        };
+
+        function submitVideo() {
+            if (vm.currentVideo.name && vm.currentVideo.link) {
+                vm.lecture.videoLinks.push(vm.currentVideo);
+                vm.currentVideo = {};
+            }
+        }
+
+        function submitSlide() {
+            if (vm.currentSlide.name && vm.currentSlide.link) {
+                vm.lecture.slidesLinks.push(vm.currentSlide);
+                vm.currentSlide = {};
+            }
+        }
+
+        function submitRepository() {
+            if (vm.currentRepository.name && vm.currentRepository.link) {
+                vm.lecture.repositoryLinks.push(vm.currentRepository);
+                vm.currentRepository = {};
+            }
+        }
+
+        function submitSample() {
+            if (vm.currentSample.name && vm.currentSample.link) {
+                vm.lecture.sampleLinks.push(vm.currentSample);
+                vm.currentSample = {};
+            }
+        }
+
+        function submitUseful() {
+            if (vm.currentuseful.name && vm.currentuseful.link) {
+                vm.lecture.usefulLinks.push(vm.currentuseful);
+                vm.currentuseful = {};
             }
         }
     }
