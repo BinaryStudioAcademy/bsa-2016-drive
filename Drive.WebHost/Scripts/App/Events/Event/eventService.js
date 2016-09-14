a29438e719cf0af3dbd13ac5a19e617625103303
Artur Brutjan <artur.brutjan7@gmail.com>
2016-09-15 02:55:09 +0300

    #391: Implemented event service

--- Drive.WebHost/Scripts/App/Events/Event/eventService.js
+++ Drive.WebHost/Scripts/App/Events/Event/eventService.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('driveApp.academyPro')
+        .module('driveApp.events')
         .factory('EventService', EventService);
 
     EventService.$inject = [
@@ -12,9 +12,23 @@
 
     function EventService($http, baseUrl) {
         var service = {
-            
-        }
+            getEvent: getEvent
+        };
 
         return service;
+
+        function getEvent(id) {
+            return $http.get(baseUrl + '/api/events/' + id)
+                   .then(getEventCompleted)
+                   .catch(getEventFailed);
+
+            function getEventCompleted(response) {
+                return response.data;
+            }
+
+            function getEventFailed(error) {
+                console.log('XHR Failed for getEvent.' + error.data);
+            }
+        }
     }
 })();
