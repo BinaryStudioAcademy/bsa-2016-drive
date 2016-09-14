e4fd3affd20684a2d5f8b86b090c02ae6992363c
Artur Brutjan <artur.brutjan7@gmail.com>
2016-09-14 12:49:42 +0300

    #391: Basic event display module structure added

---
+++ Drive.WebHost/Scripts/App/Events/Event/eventController.js
@@ -0,0 +1,21 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('driveApp.academyPro')
+        .controller('EventController', EventController);
+
+    EventController.$inject = [
+        'eventService'
+    ];
+
+    function EventController(eventService) {
+        var vm = this;
+
+        activate();
+
+        function activate() {
+            
+        }
+    }
+})();
---
+++ Drive.WebHost/Scripts/App/Events/Event/eventService.js
@@ -0,0 +1,20 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('driveApp.academyPro')
+        .factory('EventService', EventService);
+
+    EventService.$inject = [
+        '$http',
+        'BaseUrl'
+    ];
+
+    function EventService($http, baseUrl) {
+        var service = {
+            
+        }
+
+        return service;
+    }
+})();
