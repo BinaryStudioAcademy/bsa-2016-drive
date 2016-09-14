6c01785b0de74bc3de440efbd46863ac3497a9df
Artur Brutjan <artur.brutjan7@gmail.com>
2016-09-15 02:55:30 +0300

    #391: Implemented event controller

--- Drive.WebHost/Scripts/App/Events/Event/eventController.js
+++ Drive.WebHost/Scripts/App/Events/Event/eventController.js
@@ -2,20 +2,49 @@
     'use strict';
 
     angular
-        .module('driveApp.academyPro')
+        .module('driveApp.events')
         .controller('EventController', EventController);
 
     EventController.$inject = [
-        'eventService'
+        'EventService',
+        '$routeParams',
+        '$sce',
+        'ContentTypeService'
     ];
 
-    function EventController(eventService) {
+    function EventController(eventService, $routeParams, $sce, contentTypeService) {
         var vm = this;
+        vm.currentEventId = $routeParams.id;
+        vm.event = null;
+        vm.getEvent = getEvent;
+        vm.trustSrc = trustSrc;
+        vm.contentTypes = contentTypeService;
+        
 
         activate();
 
         function activate() {
-            
+            vm.sortedContentList = [];
+            return getEvent();
+        }
+
+        function getEvent() {
+            return eventService.getEvent(vm.currentEventId)
+                .then(function (data) {
+                    vm.event = data;
+                    vm.sortedContentList = vm.sortedContentList.concat(vm.event.contentVideoLinks);
+                    vm.sortedContentList = vm.sortedContentList.concat(vm.event.contentSimpleLinks);
+                    vm.sortedContentList = vm.sortedContentList.concat(vm.event.contentPhotos);
+                    vm.sortedContentList = vm.sortedContentList.concat(vm.event.contentTexts);
+                    vm.sortedContentList.sort(function (a, b) {
+                        return a.order - b.order;
+                    });
+                    return vm.event;
+                });
+        }
+
+        function trustSrc(src) {
+            return $sce.trustAsResourceUrl(src);
         }
     }
 })();
