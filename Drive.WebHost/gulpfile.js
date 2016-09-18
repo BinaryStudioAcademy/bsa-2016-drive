d59105818401f14effcc358824f72f7fafab22f2
Artur Brutjan <artur.brutjan7@gmail.com>
2016-09-18 22:58:41 +0300

    #462: Added angular-jwt lib

--- Drive.WebHost/gulpfile.js
+++ Drive.WebHost/gulpfile.js
@@ -73,7 +73,8 @@ gulp.task("libs", function () {
       paths.bower + "angular-drag-and-drop-lists.js",
       paths.bower + "build/pdf.combined.js",
       paths.bower + "dist/angular-pdf-viewer.min.js",
-      paths.bower + "ng-videosharing-embed.min.js"
+      paths.bower + "ng-videosharing-embed.min.js",
+      paths.bower + "angular-jwt.min.js"
     ])
 
         .pipe(concat("libs.js"))
