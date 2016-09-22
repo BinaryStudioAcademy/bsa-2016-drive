63e47d9554a5d2f70628386806a44db5972bb7c0
SpottyLenopard <spotty.lenopard@gmail.com>
2016-09-22 12:34:28 +0300

    394 file download

--- Drive.WebHost/Controllers/HomeController.cs
+++ Drive.WebHost/Controllers/HomeController.cs
@@ -2,15 +2,20 @@
 using System.Threading.Tasks;
 using Drive.WebHost.Services;
 using System;
+using Drive.Logging;
 
 namespace Drive.WebHost.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ISharedByLinkService _sharedByLinkService;
-        public HomeController(ISharedByLinkService service)
+        private readonly IFileService _fileService;
+        private readonly ILogger _logger;
+        public HomeController(ISharedByLinkService service, IFileService fs, ILogger logger)
         {
             _sharedByLinkService = service;
+            _fileService = fs;
+            _logger = logger;
         }
         public ActionResult Index()
         {
@@ -24,6 +29,25 @@ namespace Drive.WebHost.Controllers
             ViewBag.BasePath = System.Configuration.ConfigurationManager.AppSettings["basePath"];
             var contentList = await _sharedByLinkService.GetContentByLink(Id);
             return View(contentList);
+        }
+
+        [AllowAnonymous]
+        public async Task<ActionResult> Download(string fileLink)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(fileLink))
+                {
+                    var fileStreamContainer = await _fileService.DownloadFile(fileLink);
+                    if (fileStreamContainer != null)
+                        return File(fileStreamContainer.Content, fileStreamContainer.Type, fileStreamContainer.Name);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.WriteError(ex, ex.Message);
+            }
+            return View("Error");
         }
     }
 }
